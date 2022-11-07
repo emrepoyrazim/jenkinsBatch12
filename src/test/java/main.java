185bbc74725ebc13a59e45e5295b61a552c6d694
emre <emrepoyrazim@gmail.com>
2022-11-08 02:53:30 +0300

    2nd commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         for(int i=0; i<5; i++){
             System.out.println("hello! am i a cat");
+            System.out.println("hello i am a dog");
         }
 
     }
